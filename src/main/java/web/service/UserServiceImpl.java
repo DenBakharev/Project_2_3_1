44d26e8e23e90b8f56f162c0440c414ec0464c3e
Denis Bakharev <java.bakharev@gmail.com>
2023-04-11 12:03:31 +0300

    correct Transactional

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package web.service;
 
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
@@ -17,26 +18,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
+    @Transactional
     public void deleteUserById(long id) {
         userDao.deleteUserById(id);
     }
 
     @Override
+    @Transactional
     public void updateUserById(long id, User user) {
         userDao.updateUserById(id, user);
     }
 
     @Override
+    @Transactional
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
 
     @Override
+    @Transactional
     public List<User> getListOfUsers() {
         return userDao.getListOfUsers();
     }
